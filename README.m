0909b1991ad218c2997ec55f2fc51cdbfc125cb3
Galaxy-Coding <69968484+KimYeoungChan@users.noreply.github.com>
2023-01-04 16:53:46 +0900

    Create README.m

---
+++ README.m
@@ -0,0 +1,16 @@
+# Nomad Coder
+
+바닐라 JS로 크롬 앱 만들기
+
+
+
+## 템플릿 리터럴(Template literals)
+ `${}`
+ 
+## Window.LocalStorage method
+localStorage.setItem(); // 어플리케이션 탭에 localStorage를 적용
+localStorage.getItem(); // 어플리케이션 탭에 localStorage를 읽음
+localStorage.removeItem(); // 어플리케이션 탭에 localStorage를 제거
+
+## setInterval function()
+setInterval ( 함수이름 , 시간 );
